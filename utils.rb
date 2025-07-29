6cc64b31834352370469fded3a14f8e20f5568a6
Masahiro Nakao <masahiro.nakao@riken.jp>
2025-07-29 13:45:41 +0900

    Upper limit of memory is updated

--- utils.rb
+++ utils.rb
@@ -185,9 +185,9 @@ def prepost_common()
     label: Partition
     required: true
     options:
-      - [ gpu1, gpu1, set-max-prepost_time_1: 3, set-label-prepost_time_1: Maximum hours (0 - 3), set-max-prepost_cores:  72, set-label-prepost_cores: Number of CPU cores (1 - 72),  set-max-prepost_memory:  186, set-label-prepost_memory: Memory (5 - 186GB)  ]
-      - [ gpu2, gpu2,                                                                             set-max-prepost_cores:  36, set-label-prepost_cores: Number of CPU cores (1 - 36),  set-max-prepost_memory:   93, set-label-prepost_memory: Memory (5 - 93GB)   ]
-      - [ mem1, mem1, set-max-prepost_time_1: 3, set-label-prepost_time_1: Maximum hours (0 - 3), set-max-prepost_cores: 224, set-label-prepost_cores: Number of CPU cores (1 - 224), set-max-prepost_memory: 5020, set-label-prepost_memory: Memory (5 - 5020GB) ]
+      - [ gpu1, gpu1, set-max-prepost_time_1: 3, set-label-prepost_time_1: Maximum hours (0 - 3), set-max-prepost_cores:  72, set-label-prepost_cores: Number of CPU cores (1 - 72),  set-max-prepost_memory:  181, set-label-prepost_memory: Memory (5 - 181GB)  ]
+      - [ gpu2, gpu2,                                                                             set-max-prepost_cores:  36, set-label-prepost_cores: Number of CPU cores (1 - 36),  set-max-prepost_memory:   90, set-label-prepost_memory: Memory (5 - 90GB)   ]
+      - [ mem1, mem1, set-max-prepost_time_1: 3, set-label-prepost_time_1: Maximum hours (0 - 3), set-max-prepost_cores: 224, set-label-prepost_cores: Number of CPU cores (1 - 224), set-max-prepost_memory: 5001, set-label-prepost_memory: Memory (5 - 5001GB) ]
       - [ mem2, mem2,                                                                             set-max-prepost_cores:  56, set-label-prepost_cores: Number of CPU cores (1 - 56),  set-max-prepost_memory: 1500, set-label-prepost_memory: Memory (5 - 1500GB) ]
 
   prepost_time:
